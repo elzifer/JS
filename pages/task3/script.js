87c85b5b41fd01da4edc6a46cde369f34c0e25f9
El Popkova <el.balzak@gmail.com>
2022-09-21 16:30:13 +0300

    reload css

--- pages/task3/script.js
+++ pages/task3/script.js
@@ -1,13 +1,14 @@
-
 const trafficLightEl = document.querySelector('#trafficLight');
+
+trafficLightEl.addEventListener('click', makeGreen);
+trafficLightEl.addEventListener('click', makeYellow);
+trafficLightEl.addEventListener('click', makeRed);
 
 function makeGreen() {
     trafficLightEl.style.background = ('green');
     trafficLightEl.removeEventListener('click', makeGreen);
-trafficLightEl.addEventListener('click', makeYellow);
+    trafficLightEl.addEventListener('click', makeYellow);
 }
-
-trafficLightEl.addEventListener('click', makeGreen);
 
 function makeYellow() {
     trafficLightEl.style.background = ('yellow');
@@ -15,11 +16,10 @@ function makeYellow() {
     trafficLightEl.addEventListener('click', makeRed);
 }
 
-trafficLightEl.addEventListener('click', makeYellow);
-
 function makeRed() {
     trafficLightEl.style.background = ('red');
     trafficLightEl.removeEventListener('click', makeRed);
     trafficLightEl.addEventListener('click', makeGreen);
 }
-trafficLightEl.addEventListener('click', makeRed);
+
+
